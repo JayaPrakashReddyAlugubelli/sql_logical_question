df9f81a15a3ff1d599de44503fd60abe36c055f9
JayaPrakashReddyAlugubelli <106681163+JayaPrakashReddyAlugubelli@users.noreply.github.com>
2024-07-18 14:06:02 +0530

    Update 20240715.sql

--- 20240715.sql
+++ 20240715.sql
@@ -15,3 +15,11 @@ INSERT INTO EmployeeSkills (Col1, Col2) VALUES ('hari', 'x.y.z');
 |hari|x.y.z|x   |y   |z   |
 +----+-----+----+----+----+
 */
+
+
+---my solution---
+
+SELECT * , SUBSTRING_INDEX(Col2, '.', 1) as Col3,
+           SUBSTRING_INDEX(SUBSTRING_INDEX(Col2, '.', 2), '.', -1) AS Col4,
+			  SUBSTRING_INDEX(Col2, '.', -1) AS Col5
+from employeeskills;
