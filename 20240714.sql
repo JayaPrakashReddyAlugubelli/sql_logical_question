3cd7df8287a3314b1d4abdd346190f7546808db9
JayaPrakashReddyAlugubelli <106681163+JayaPrakashReddyAlugubelli@users.noreply.github.com>
2024-07-19 10:44:53 +0530

    Create 20240714.sql

---
+++ 20240714.sql
@@ -0,0 +1,27 @@
+CREATE TABLE CricketTeams (
+    Id INT PRIMARY KEY,
+    TeamName VARCHAR(100)
+);
+
+INSERT INTO CricketTeams (Id, TeamName) VALUES
+(1, 'India'),
+(2, 'Australia'),
+(3, 'England'),
+(4, 'NewZealand');
+/*
+Sql query to schedule a match between each teams
++-----------------------+
+|schedule_match         |
++-----------------------+
+|India VS Australia     |
+|India VS England       |
+|India VS NewZealand    |
+|Australia VS England   |
+|Australia VS NewZealand|
+|England VS NewZealand  |
++-----------------------+
+*/
+
+-----my solution ---
+SELECT a.TeamName ,"vs",b.TeamName FROM CricketTeams a 
+INNER JOIN CricketTeams b ON a.TeamName < b.TeamName   ;
