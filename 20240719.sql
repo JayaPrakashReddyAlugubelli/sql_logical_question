af6ae03dc5cedd8a37d9225d10cb5a5273522b27
JayaPrakashReddyAlugubelli <106681163+JayaPrakashReddyAlugubelli@users.noreply.github.com>
2024-07-19 09:41:33 +0530

    Create 20240719.sql

---
+++ 20240719.sql
@@ -0,0 +1,39 @@
+CREATE TABLE EmployeeData (
+    Dep VARCHAR(50),
+    Gender CHAR(1)
+);
+
+INSERT INTO EmployeeData (Dep, Gender) VALUES
+('IT', 'M'),
+('IT', 'F'),
+('IT', 'M'),
+('IT', 'M'),
+('HR', 'F'),
+('HR', 'F'),
+('HR', 'F'),
+('HR', 'F'),
+('HR', 'F'),
+('Sales', 'M'),
+('Sales', 'M'),
+('Sales', 'F'),
+('Sales', 'M'),
+('Sales', 'M'),
+('Sales', 'F');
+
+/*
+find out toatl employes in each dep ,how many male emps and female emps
+output:-
++-----+---+----+------+
+|  Dep|Dep|Male|Female|
++-----+---+----+------+
+|   IT|  4|   3|     1|
+|   HR|  5|null|     5|
+|Sales|  6|   4|     2|
++-----+---+----+------+
+*/
+
+---my solustion
+
+SELECT Dep,COUNT(gender),sum(male_emp),sum(Female_emp) FROM 
+(SELECT *,case when gender='M' then 1 else 0 END AS male_emp , 
+case when gender='F' then 1 else 0 END AS Female_emp FROM EmployeeData) AS k GROUP BY Dep ;
