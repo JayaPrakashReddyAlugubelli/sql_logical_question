1c964a2510ceecdf4186181cbdcc9b738da7e600
JayaPrakashReddyAlugubelli <106681163+JayaPrakashReddyAlugubelli@users.noreply.github.com>
2024-09-04 06:00:34 +0530

    All

--- 20240902_1.sql
+++ 20240902_1.sql
@@ -8,13 +8,13 @@ insert into cinema_tbl values (1,1),(2,0),(3,1),(4,0),(5,1),(6,1),(7,1),(8,0),(9
 
 
 select distinct a.seat_id from cinema_tbl a join cinema_tbl b on abs(a.seat_id - b.seat_id)=1 and a.free=1 and b.free=1
-
+--------------------
 
 with CTE as (
 select * , free * lag(free,1,0) over(order by seat_id) as lags, 
 free * lead(free,1,0) over(order by seat_id) as leads from cinema_tbl)
 select seat_id from CTE where lags=1 or leads=1;
-
+--------------------
 
 WITH
     T AS (
---
+++ 20240903.sql
@@ -0,0 +1,26 @@
+Given us brands table, we need to forward fill the categories wherever we have nulls for all brands in the table.
+
+create table brands (category varchar(50), brand_name varchar(50))
+------------
+
+insert into brands values ('chocolates', '5-star'),(NULL, 'dairy milk'),(NULL, 'perk'),(NULL, 'eclair'),('Biscuits', 'Britania'),(NULL, 'good day'),(NULL, 'boost')
+----------
+
+WITH cte_1 as(
+SELECT * ,ROW_NUMBER() OVER(ORDER BY (SELECT NULL)) AS rn FROM brands)
+, cte_2 AS (
+SELECT *,COUNT(category ) OVER(ORDER BY rn ) AS ct FROM cte_1)
+SELECT first_value(category) OVER(partition BY ct ORDER BY rn),brand_name  FROM cte_2;
+------
+
+SELECT 
+    COALESCE(b.category, prev_categories.last_non_null_category) AS filled_category,
+    b.brand_name
+FROM brands b
+OUTER APPLY (
+    SELECT TOP 1 category AS last_non_null_category
+    FROM brands b2
+    WHERE b2.brand_name <= b.brand_name
+      AND b2.category IS NOT NULL
+    ORDER BY b2.brand_name
+) prev_categories;
